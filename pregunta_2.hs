3ebbbd090b4cd83d7015b21793d73f2ea9c153d3
Alessandro <alessandroempire@gmail.com>
2015-01-10 18:38:51 -0430

    pregunta 2 listo

--- pregunta_2.hs
+++ pregunta_2.hs
@@ -1,4 +1,18 @@
+import              System.Environment      (getArgs)
+import              Data.List               
 
 main = do
-    (fileName : ) <- getArgs
+    (fileName : _ ) <- getArgs
     input <- readFile fileName
+    let dup  = nub $ concat $ map duplicados (lines input)
+        all  = ['A' .. 'Z']
+        ans  = all \\ dup
+    print $ "Las letras que nunca aparecen repetidas son:"
+    mapM_ print ans
+
+duplicados :: Eq a => [a] -> [a] 
+duplicados []  = []
+duplicados [x] = []
+duplicados (x : xs) 
+    | x == head xs  = [x] ++ duplicados (tail xs)
+    | otherwise     = duplicados xs 
